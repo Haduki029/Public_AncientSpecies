1baacf24b318577203d31be0ce63a3bb98014099
Haduki <haduki@dummymail.com>
2020-06-28 11:50:51 +0900

    Initial commit
    Add the new species that's name is Weeb Half Elf.

---
+++ Source/WHE/WHE/DefOf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using AlienRace;
+using Verse;
+
+namespace AS_WHE
+{
+    [DefOf]
+    public static class AS_WHE_DefOf
+    {
+        public static ThingDef_AlienRace AS_WHE_Race;
+
+        public static ScenarioDef AS_WHE_Scenario;
+
+        public static PawnKindDef AS_WHE_Colonist;
+
+        public static PawnKindDef AS_WHE_Citizen;
+    }
+}
---
+++ Source/WHE/WHE/Settings.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace AS_WHE
+{
+    [StaticConstructorOnStartup]
+    class PatchSetClass
+    {
+        static PatchSetClass()
+        {
+            Harmony harmonyInstance = new Harmony("com.ColorPatch.rimworld.mod");
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnApparelGenerator))]
+    [HarmonyPatch("GenerateStartingApparelFor")]
+    public static class PawnGenColorClass
+    {
+        [HarmonyPostfix]
+        private static void Postfix(ref Pawn pawn)
+        {
+            if (pawn.apparel != null)
+            {
+                List<Apparel> wornApparel = pawn.apparel.WornApparel;
+                for (int i = 0; i < wornApparel.Count; i++)
+                {
+                    CustomThingDef colorDef = wornApparel[i].def as CustomThingDef;
+                    if (colorDef != null && !colorDef.followStuffColor)
+                    {
+                        CompColorableUtility.SetColor(wornApparel[i], Color.white, true);
+                        CompColorableUtility.SetColor(wornApparel[i], Color.black, true);
+                        CompColorableUtility.SetColor(wornApparel[i], Color.white, true);
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ThingMaker))]
+    [HarmonyPatch("MakeThing")]
+    public static class ThingMakeColorClass
+    {
+        [HarmonyPostfix]
+        static void Postfix(ref Thing __result)
+        {
+            ThingWithComps twc = __result as ThingWithComps;
+            if (twc != null)
+            {
+                CustomThingDef colorDef = twc.def as CustomThingDef;
+                if (colorDef != null && !colorDef.followStuffColor)
+                {
+                    twc.SetColor(Color.white);
+                    twc.SetColor(Color.black);
+                    twc.SetColor(Color.white);
+                }
+            }
+        }
+    }
+
+    public class CustomThingDef : ThingDef
+    {
+        public bool followStuffColor = true;
+    }
+}
