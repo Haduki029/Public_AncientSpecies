30e37b0d3cd226ef2f5f62bd08f13b1df5a3723c
Haduki <haduki@dummymail.com>
2020-10-26 17:05:05 +0900

    修正の一時取り込み

---
+++ Source/WHE/WHE/HarmonyPatch/IncidentsPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using HarmonyLib;
+
+namespace AS_WHE.HarmonyPatch
+{
+    public static class ASWHEPatch
+    {
+        private static readonly Type patchType = typeof(ASWHEPatch);
+        static ASWHEPatch()
+        {
+            Harmony harmonyInstance = new Harmony("rimworld.as.whe.mod");
+            harmonyInstance.Patch(AccessTools.Method(typeof(IncidentWorker_WildManWandersIn), "TryExecuteWorker", null, null), new HarmonyMethod(patchType, "TryExecuteWorkerPrefix", null), null, null);
+        }
+
+    }
+}
